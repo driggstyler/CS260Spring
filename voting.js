89d38d055919cac239e95b502d93e67e9df7ce2e
driggstyler <driggstyler@gmail.com>
2023-06-16 00:09:41 -0600

    Stopped on array storing/iterating

---
+++ voting.js
@@ -0,0 +1,24 @@
+let points = 0;
+document.getElementById('currUsername').innerHTML= localStorage.getItem('username');
+
+let myArray = ['1','2','3','4','5','6','7','8','9','10','11','12'];
+
+document.getElementById('usersWord').innerHTML = myArray.pop;
+document.getAnimations('currItem').innerHTML = "Things that are round";
+
+function yesButton() {
+    points++;
+    document.getElementById('usersWord').innerHTML = myArray.pop;
+    document.getAnimations('currItem').innerHTML = "Things that are round";
+    if (myArray.length === 0) {
+        window.location.ref = "endgame.html"
+    }
+}
+
+function noButton() {
+    document.getElementById('usersWord').innerHTML = myArray.pop;
+    document.getAnimations('currItem').innerHTML = "Things that are round";
+    if (myArray.length === 0) {
+        window.location.ref = "endgame.html"
+    }
+}
